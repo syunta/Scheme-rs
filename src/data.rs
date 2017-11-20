a3b0cf02c7bff793432ad3fdd903fde1b9fcd6f6
syunta <kitagawa@timedia.co.jp>
2017-11-20 21:14:01 +0900

    Add Cell

--- src/data.rs
+++ src/data.rs
@@ -1,15 +1,28 @@
 use std::fmt;
+use std::rc::Rc;
 
 pub enum Obj {
     Nil,
-    Int(i32)
+    Int(i32),
+    Cell(Rc<Obj>, Rc<Obj>)
 }
 
 impl fmt::Display for Obj {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             Obj::Nil => write!(f, "{}", "()"),
-            Obj::Int(i) => write!(f, "{}", i)
+            Obj::Int(i) => write!(f, "{}", i),
+            Obj::Cell(ref car, ref cdr) => {
+                write!(f, "(").unwrap();
+                write!(f, "{}", car).unwrap();
+
+                match **cdr {
+                    Obj::Nil  => write!(f, "").unwrap(),
+                    ref other => write!(f, " . {}", other).unwrap()
+                }
+
+                return write!(f, ")");
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod data;
+use std::rc::Rc;
 
 fn main() {
-    let hoge = data::Obj::Nil;
-    let huga = data::Obj::Int(100);
+    let hoge = data::Obj::Cell(Rc::new(data::Obj::Int(20)), Rc::new(data::Obj::Nil));
+    let huga = data::Obj::Cell(Rc::new(data::Obj::Int(20)), Rc::new(data::Obj::Int(100)));
     println!("{}", hoge);
     println!("{}", huga);
 }
