46e82520d92ed0b2e29f2ef047ac46eb7782ad09
syunta <kitagawa@timedia.co.jp>
2017-11-21 20:53:45 +0900

    Enable to show recursive Cell

--- src/data.rs
+++ src/data.rs
@@ -15,14 +15,20 @@ impl fmt::Display for Obj {
             Obj::Cell(ref car, ref cdr) => {
                 write!(f, "(").unwrap();
                 write!(f, "{}", car).unwrap();
-
-                match **cdr {
-                    Obj::Nil  => write!(f, "").unwrap(),
-                    ref other => write!(f, " . {}", other).unwrap()
-                }
-
+                print_cdr(cdr, f).unwrap();
                 return write!(f, ")");
             }
         }
+    }
+}
+
+fn print_cdr(obj: &Obj, f: &mut fmt::Formatter) -> fmt::Result {
+    match *obj {
+        Obj::Nil => write!(f, ""),
+        Obj::Cell(ref car, ref cdr) => {
+            write!(f, " {}", car).unwrap();
+            print_cdr(cdr, f)
+        },
+        ref atom => write!(f, " . {}", atom)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ mod data;
 use std::rc::Rc;
 
 fn main() {
-    let hoge = data::Obj::Cell(Rc::new(data::Obj::Int(20)), Rc::new(data::Obj::Nil));
-    let huga = data::Obj::Cell(Rc::new(data::Obj::Int(20)), Rc::new(data::Obj::Int(100)));
-    println!("{}", hoge);
-    println!("{}", huga);
+    let hoge = data::Obj::Cell(Rc::new(data::Obj::Int(10)), Rc::new(data::Obj::Nil));
+    let huga = data::Obj::Cell(Rc::new(data::Obj::Int(20)), Rc::new(hoge));
+    let piyo = data::Obj::Cell(Rc::new(data::Obj::Int(30)), Rc::new(huga));
+    println!("{}", piyo);
 }
