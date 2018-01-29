b9d3ac8fc7bb25028c44212e0f1279d5816a73f6
syunta <kitagawa@timedia.co.jp>
2018-01-29 21:03:04 +0900

    Add test for mod data

--- src/data.rs
+++ src/data.rs
@@ -35,3 +35,17 @@ fn format_cdr(obj: &Obj) -> String {
         ref atom => " . ".to_string() + &atom.to_string()
     }
 }
+
+#[test]
+fn it_works() {
+    let a = Obj::Cell(Rc::new(Obj::Int(10)), Rc::new(Obj::Nil));
+    let b = Obj::Cell(Rc::new(Obj::Int(20)), Rc::new(a));
+    let c = Obj::Cell(Rc::new(Obj::Int(30)), Rc::new(b));
+    assert_eq!("(30 20 10)", format_string(&c));
+
+    let d = Obj::Cell(Rc::new(Obj::Int(1)), Rc::new(Obj::Int(2)));
+    assert_eq!("(1 . 2)", format_string(&d));
+
+    let e = Obj::Cell(Rc::new(c), Rc::new(d));
+    assert_eq!("((30 20 10) 1 . 2)", format_string(&e));
+}
