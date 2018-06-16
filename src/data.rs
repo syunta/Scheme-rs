5e3d09335bc7a0bc5390dbbd2f105db30144d381
syunta <kitagawa@timedia.co.jp>
2018-06-16 16:24:34 +0900

    Reset

--- src/data.rs
+++ src/data.rs
@@ -1,52 +1,11 @@
 use std::fmt;
 use std::rc::Rc;
 
-#[derive(PartialEq, Debug)]
 pub enum Obj {
     Nil,
     Int(i32),
-    Cell(Rc<Obj>, Rc<Obj>)
-}
-
-impl fmt::Display for Obj {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", format_string(self))
-    }
-}
-
-fn format_string(obj: &Obj) -> String {
-    match *obj {
-        Obj::Nil => "()".to_string(),
-        Obj::Int(i) => i.to_string(),
-        Obj::Cell(ref car, ref cdr) => {
-            "(".to_string()
-                + &format_string(car)
-                + &format_cdr(cdr)
-                + ")"
-        }
-    }
-}
-
-fn format_cdr(obj: &Obj) -> String {
-    match *obj {
-        Obj::Nil => "".to_string(),
-        Obj::Cell(ref car, ref cdr) => {
-            " ".to_string() + &car.to_string() + &format_cdr(cdr)
-        },
-        ref atom => " . ".to_string() + &atom.to_string()
-    }
 }
 
 #[test]
 fn it_works() {
-    let a = Obj::Cell(Rc::new(Obj::Int(10)), Rc::new(Obj::Nil));
-    let b = Obj::Cell(Rc::new(Obj::Int(20)), Rc::new(a));
-    let c = Obj::Cell(Rc::new(Obj::Int(30)), Rc::new(b));
-    assert_eq!("(30 20 10)", format_string(&c));
-
-    let d = Obj::Cell(Rc::new(Obj::Int(1)), Rc::new(Obj::Int(2)));
-    assert_eq!("(1 . 2)", format_string(&d));
-
-    let e = Obj::Cell(Rc::new(c), Rc::new(d));
-    assert_eq!("((30 20 10) 1 . 2)", format_string(&e));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-mod data;
-mod parser;
-use std::rc::Rc;
-
 fn main() {
-    let hoge = data::Obj::Cell(Rc::new(data::Obj::Int(10)), Rc::new(data::Obj::Nil));
-    let huga = data::Obj::Cell(Rc::new(data::Obj::Int(20)), Rc::new(hoge));
-    let piyo = data::Obj::Cell(Rc::new(data::Obj::Int(30)), Rc::new(huga));
-    println!("{}", piyo);
 
-    let mut p = parser::Parser { unparsed: "10".to_string() };
-    p.parse();
 }
